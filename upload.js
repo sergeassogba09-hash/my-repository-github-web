c956673a8ba87a3ef082bd83b6aa37705ff477a4
sergeassogba09-hash <sergeassogba09@gmail.com>
2025-09-02 15:25:03 +0000

    Delete upload.js

--- upload.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { uploadVideo } = require('videoController');
-
-router.post('/', uploadVideo);
-module.exports = router;
